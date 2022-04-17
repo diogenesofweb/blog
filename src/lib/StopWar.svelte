bfbd729c9cd16a89ff1f2de94fdb3b2eaada8522
Diogenes <diogenesofweb@gmail.com>
2022-04-17 09:17:01 +0300

    up

--- src/lib/StopWar.svelte
+++ src/lib/StopWar.svelte
@@ -1,13 +1,13 @@
 <section id="stop-war">
-  <a href="https://ukraine.ua/news/stand-with-ukraine/" target="_blank" rel="noopener noreferrer">
-    <span>STOP WAR. STAND WITH UKRAINE 🇺🇦</span>
+  <a href="https://war.ukraine.ua/support-ukraine/" target="_blank" rel="noopener noreferrer">
+    <span>STAND WITH UKRAINE 🇺🇦</span>
   </a>
 </section>
 
 <style>
   #stop-war {
-    position: fixed;
     z-index: 999;
+    position: fixed;
     bottom: 0;
     left: 0;
     right: 0;
@@ -16,9 +16,13 @@
     font-size: 16px;
     text-align: center;
   }
-
   #stop-war a {
-    color: red;
+    font-family: 'Courier New', Courier, monospace;
+    /* color: red; */
+    color: salmon;
     text-decoration: none;
+  }
+  #stop-war a:is(:hover, :focus) {
+    text-decoration: underline;
   }
 </style>
