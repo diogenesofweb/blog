f6884673bf460a6ab0992984c70269f15af3a7fe
Diogenes <diogenesofweb@gmail.com>
2023-02-11 14:06:30 +0200

    up

--- src/lib/StopWar.svelte
+++ src/lib/StopWar.svelte
@@ -4,7 +4,12 @@
 		target="_blank"
 		rel="noopener noreferrer"
 	>
-		<span>STAND WITH UKRAINE 🇺🇦</span>
+		<span>STAND WITH UKRAINE </span>
+
+		<svg width="30" height="20">
+			<rect width="30" height="20" fill="#005BBB" />
+			<rect width="30" height="10" y="10" fill="#FFD500" />
+		</svg>
 	</a>
 </section>
 
@@ -15,7 +20,8 @@
 		bottom: 0;
 		left: 0;
 		right: 0;
-		padding: 1rem;
+		line-height: 1;
+		padding: 1.5ch;
 		background-color: black;
 		font-size: 16px;
 		text-align: center;
@@ -25,6 +31,10 @@
 		/* color: red; */
 		color: salmon;
 		text-decoration: none;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 1ch;
 	}
 	#stop-war a:is(:hover, :focus) {
 		text-decoration: underline;
