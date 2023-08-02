b8a476513316b68b9783d837ddeeaa811c30ed67
Diogenes <diogenesofweb@gmail.com>
2023-08-02 15:58:11 +0300

    adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+// import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>'],
-			},
+			fallback: '404.html', // may differ from host to host
 		}),
+		// adapter: adapter({
+		// 	routes: {
+		// 		include: ['/*'],
+		// 		exclude: ['<all>'],
+		// 	},
+		// }),
 	},
 };
 
